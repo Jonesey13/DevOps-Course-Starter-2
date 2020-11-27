5d094177f0b8946d585b7f4a6d8b4e69469e77f8
Alex Jones <alexander.jones@softwire.com>
2020-11-27 11:26:05 +0000

    Exercise 1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
+import todo_app.data.session_items as session_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,8 +9,16 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session_items.get_items()
 
+    return render_template("index.html", items=items)
+
+
+@app.route('/add', methods = ["POST"])
+def add_todo():
+    title = request.form["title"]
+    session_items.add_item(title)
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run()
